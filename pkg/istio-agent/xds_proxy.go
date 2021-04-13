6043e2a27a9dc959f1cfc89f5e5c07f67418441c
John Howard <howardjohn@google.com>
2021-04-13 15:33:43 -0700

    Switch XDS proxy tap to port 15004 (#32157)
    There are already some usages of port 15009 (for BTS) that may lead to conflict. Switching now to be safe since it hasn't shipped yet

--- pkg/istio-agent/xds_proxy.go
+++ pkg/istio-agent/xds_proxy.go
@@ -847,7 +847,7 @@ func (p *XdsProxy) makeTapHandler() func(w http.ResponseWriter, req *http.Reques
 	}
 }
 
-// initDebugInterface() listens on localhost:15009 for path /debug/...
+// initDebugInterface() listens on localhost:15004 for path /debug/...
 // forwards the paths to Istiod as xDS requests
 // waits for response from Istiod, sends it as JSON
 func (p *XdsProxy) initDebugInterface() error {
@@ -859,7 +859,7 @@ func (p *XdsProxy) initDebugInterface() error {
 	httpMux.HandleFunc("/debug", handler) // For 1.10 Istiod which uses istio.io/debug
 
 	p.httpTapServer = &http.Server{
-		Addr:    "localhost:15009",
+		Addr:    "localhost:15004",
 		Handler: httpMux,
 	}
 
